55dbf1d7dd7ad87a9480248db1d70ca956b19c7f
Your Name <you@example.com>
2021-04-14 23:55:39 +0000

    commit from va3272va project start-django

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import index
+from main.views import contact
+from main.views import about
+from main.views import cars
+from main.views import food
 
 urlpatterns = [
+    path('index',index),
+    path('contact',contact),
+    path('about',about),
+    path('cars',cars),
+    path('food',food),
     path('admin/', admin.site.urls),
 ]
