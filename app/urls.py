6c408e81014ad522f552ad341556c04a0bb0fac6
Your Name <you@example.com>
2021-04-15 10:54:09 +0000

    "commit from va3272va project start-django"

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from main.views import index
+from main.views import contact
+from main.views import about
+from main.views import cars
+from main.views import food
 
 urlpatterns = [
-    path('',index),
-    path('contact',index),
-    path('about',index),
-    path('cars',index),
-    path('food',index),
-    path('admin', admin.site.urls),
+    path('', index),
+    path('index', index),
+    path('contact', contact),
+    path('about', about),
+    path('cars', cars),
+    path('food', food),
+    path('admin.html', admin.site.urls),
 ]
